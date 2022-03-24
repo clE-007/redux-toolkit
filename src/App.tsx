a08b512c174e651dec43618a8fa0ca0ff079b784
Evgeniy Salamakha <ESalamakha@alfabank.ru>
2022-03-24 15:34:46 +0300

    add toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { useAppDispatch } from './hooks/redux';
+// import { userSlice } from './store/reducers/UserSlice';
+// import { useAppSelector } from './hooks/redux';
 
 function App() {
+  // const {  } = useAppSelector(state => state.userReducer);
+  // const dispatch = useAppDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      dsds
     </div>
   );
 }
